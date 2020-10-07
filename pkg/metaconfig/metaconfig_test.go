bb66c4dfd974d44dd66e20a6d145254256d4aa05
John Calabrese <xchapter7x@gmail.com>
2020-10-07 08:01:36 -0400

    add support for multiple package in meta file
    - multiple packages should be able to be defined in
      a single meta file

--- pkg/metaconfig/metaconfig_test.go
+++ pkg/metaconfig/metaconfig_test.go
@@ -14,6 +14,11 @@ func TestMetaConfig(t *testing.T) {
 		assert.NoError(t, err, "Error reading fixture")
 		config, err := metaconfig.Load(f)
 		assert.NoError(t, err, "Error loading config")
+		assert.NotEmpty(t, config)
+
+		t.Run("should support metadata for multiple packages in a single file", func(t *testing.T) {
+			assert.Greater(t, len(config), 1)
+		})
 
 		t.Run("should support a description field", func(t *testing.T) {
 			assert.NotEmpty(t, config[0].Description)
